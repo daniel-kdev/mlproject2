94d13c9ccfdeb4c17eefc2f78ac7dee862e6a108
daniel-kdev <k99daniel@naver.com>
2025-06-08 21:31:36 +0900

    Add files via upload

---
+++ ISLP/__init__.py
@@ -0,0 +1,12 @@
+"""
+**ISLP**  is a Python library to accompany
+*Introduction to Statistical Learning with applications in Python*.
+See the `statistical learning homepage <http://statlearning.com/>`_
+for more details.
+"""
+
+from .utils import *
+
+from . import _version
+__version__ = _version.get_versions()['version']
+
